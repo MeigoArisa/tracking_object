1475ca62ce88e11171e8dcd8d8d5d6b28adcedf6
김강현 <nuclear_1@naver.com>
2019-05-25 06:58:04 +0900

    OpenCV GUI Example

--- Tracking_Object/1. Matrix structure/1.1 GUI/GUI.cs
+++ Tracking_Object/1. Matrix structure/1.1 GUI/GUI.cs
@@ -1,12 +1,118 @@
-﻿using System;
+﻿using OpenCvSharp;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tracking_Object._1._Matrix_structure._1._1_GUI
 {
-    class GUI
+    public class GUI
     {
+        public void Windows_Example()
+        {
+            Mat WindowImage = new Mat("./Resource.jpg", ImreadModes.AnyColor);
+            var openCloseWindow = new Window("OpenCVWindow", WindowMode.AutoSize, WindowImage);
+            Debug.WriteLine(Cv2.WaitKey());
+        }
+        public void MouseCallBack_Example()
+        {
+            Mat WindowImage = new Mat("./Resource.jpg", ImreadModes.AnyColor);
+            Window foo = new Window("OpenCVWindow", WindowImage); // class OpenCvSharp.CPlusPlus.Window
+
+        }
+        public void TrackBar_Example()
+        {
+            using (var src = new Mat(@"./Resource.jpg", ImreadModes.AnyDepth | ImreadModes.AnyColor))
+            using (var dst = new Mat())
+            {
+                src.CopyTo(dst);
+
+                var elementShape = MorphShapes.Rect;
+                var maxIterations = 10;
+
+                var openCloseWindow = new Window("Open/Close", image: dst);
+                var openCloseTrackbar = openCloseWindow.CreateTrackbar(
+                    name: "Iterations", value: 10, max: maxIterations * 2 + 1,
+                    callback: (pos, obj) =>
+                    {
+                        var n = pos - maxIterations;
+                        var an = n > 0 ? n : -n;
+                        var element = Cv2.GetStructuringElement(
+                                elementShape,
+                                new Size(an * 2 + 1, an * 2 + 1),
+                                new Point(an, an));
+
+                        if (n < 0)
+                        {
+                            Cv2.MorphologyEx(src, dst, MorphTypes.Open, element);
+                        }
+                        else
+                        {
+                            Cv2.MorphologyEx(src, dst, MorphTypes.Close, element);
+                        }
+
+                        Cv2.PutText(dst, (n < 0) ?
+                            string.Format("Open/Erosion followed by Dilation[{0}]", elementShape)
+                            : string.Format("Close/Dilation followed by Erosion[{0}]", elementShape),
+                            new Point(10, 15), HersheyFonts.HersheyPlain, 1, Scalar.Black);
+                        openCloseWindow.Image = dst;
+                    });
+
+
+                var erodeDilateWindow = new Window("Erode/Dilate", image: dst);
+                var erodeDilateTrackbar = erodeDilateWindow.CreateTrackbar(
+                    name: "Iterations", value: 10, max: maxIterations * 2 + 1,
+                    callback: (pos, obj) =>
+                    {
+                        var n = pos - maxIterations;
+                        var an = n > 0 ? n : -n;
+                        var element = Cv2.GetStructuringElement(
+                                elementShape,
+                                new Size(an * 2 + 1, an * 2 + 1),
+                                new Point(an, an));
+                        if (n < 0)
+                        {
+                            Cv2.Erode(src, dst, element);
+                        }
+                        else
+                        {
+                            Cv2.Dilate(src, dst, element);
+                        }
+
+                        Cv2.PutText(dst, (n < 0) ?
+                            string.Format("Erode[{0}]", elementShape) :
+                            string.Format("Dilate[{0}]", elementShape),
+                            new Point(10, 15), HersheyFonts.HersheyPlain, 1, Scalar.Black);
+                        erodeDilateWindow.Image = dst;
+                    });
+
+
+                for (; ; )
+                {
+                    var key = Cv2.WaitKey();
+
+                    if ((char)key == 27) // ESC
+                        break;
+
+                    switch ((char)key)
+                    {
+                        case 'e':
+                            elementShape = MorphShapes.Ellipse;
+                            break;
+                        case 'r':
+                            elementShape = MorphShapes.Rect;
+                            break;
+                        case 'c':
+                            elementShape = MorphShapes.Cross;
+                            break;
+                    }
+                }
+
+                openCloseWindow.Dispose();
+                erodeDilateWindow.Dispose();
+            }
+        }
     }
 }
--- Tracking_Object/1. Matrix structure/Matrix structure.cs
+++ Tracking_Object/1. Matrix structure/Matrix structure.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using OpenCvSharp;
 using System.Threading.Tasks;
 
 namespace Tracking_Object._1.Matrix_structure
 {
     class Matrix_structure
     {
+        void Matrix_Example()
+        {
+            Mat GetImage = new Mat("",ImreadModes.AnyColor);
+        }
     }
 }
